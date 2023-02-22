b9174fc8524ae9983b69e49c7555cd2d99adc27b
Nguyen Dinh Thong <nguyenthong244466666@gmail.com>
2023-02-22 13:44:33 +0700

    tiktok-ui-second

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+// Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+// Pages
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+// Public routes
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+// Private routes
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
